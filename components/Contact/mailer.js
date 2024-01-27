2c7b450041dab28705474a5bdfd80c68a580e5e3
Charlie Van Hook <vanhookch@gmail.com>
2024-01-27 12:08:46 -0500

    email

--- components/Contact/mailer.js
+++ components/Contact/mailer.js
@@ -2,10 +2,10 @@ import emailjs from "@emailjs/browser";
 
 const mail = ({ name, email, message }) => {
   return emailjs.send(
-    process.env.NEXT_PUBLIC_SERVICE_ID,
-    process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    "service_5avm3vi",
+    "template_64ipuas",
     { name, email, message },
-    process.env.NEXT_PUBLIC_USER_ID
+    "DIYG-tnEaBcHqjFDa"
   );
 };
 
